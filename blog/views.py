8992ecac7383c86162247976bd9ffac28fb8a355
starBoy-right <romiwahyudihasibuan@gmail.com>
2026-01-18 02:22:51 +0000

    first-commit: blango project

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.utils import timezone
+from blog.models import Post
+
+
+# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    return render(request, "blog/index.html", {"posts":posts})
