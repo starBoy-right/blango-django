3542cf3c937529ab4195cdd4ed1bd329ccd0cff4
starBoy-right <romiwahyudihasibuan@gmail.com>
2026-01-19 08:20:53 +0000

    Finish custom filters

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -7,11 +7,14 @@ user_model = get_user_model()
 register = template.Library()
 
 @register.filter
-def author_details(author):
+def author_details(author, current_user=None):
   if not isinstance(author, user_model):
     # return empty string as safe default
     return ""
   
+  if author == current_user:
+    return format_html("<strong>me</strong>")
+
   if author.first_name and author.last_name:
     name = f"{author.first_name} {author.last_name}"
   else:
