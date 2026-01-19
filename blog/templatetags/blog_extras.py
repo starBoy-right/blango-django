614a725b8bb59dd8c354d209f30952b058899487
starBoy-right <romiwahyudihasibuan@gmail.com>
2026-01-19 08:10:48 +0000

    update format_html to prevent xss attack

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,7 +1,9 @@
 from django import template
 from django.contrib.auth import get_user_model
-user_model = get_user_model()
+from django.utils.html import format_html
 
+
+user_model = get_user_model()
 register = template.Library()
 
 @register.filter
@@ -15,4 +17,11 @@ def author_details(author):
   else:
     name = f"{author.username}"
 
-  return name
+  if author.email:
+    prefix = format_html('<a href="mailto:{}">', author.email)
+    suffix = format_html("</a>")
+  else:
+    prefix = ""
+    suffix = ""
+
+  return format_html('{}{}{}',prefix, name, suffix)
