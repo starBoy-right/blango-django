e88d473b39bd748fa7765630d86e09c3663aec7f
starBoy-right <romiwahyudihasibuan@gmail.com>
2026-01-22 14:58:59 +0000

    update custom template tag

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -28,3 +28,13 @@ def author_details(author, current_user=None):
     suffix = ""
 
   return format_html('{}{}{}',prefix, name, suffix)
+
+
+@register.simple_tag
+def row(extra_classes=""):
+  return format_html('<div class="row {}">', extra_classes)
+
+
+@register.simple_tag
+def endrow():
+  return format_html("</div>")
