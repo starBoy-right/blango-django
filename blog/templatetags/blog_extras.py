40ea3faa76d6efb101ba04e66eaaef95fcfdc46e
starBoy-right <romiwahyudihasibuan@gmail.com>
2026-01-23 01:59:40 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,6 +1,7 @@
 from django import template
 from django.contrib.auth import get_user_model
 from django.utils.html import format_html
+from blog.models import Post
 
 
 user_model = get_user_model()
@@ -38,3 +39,19 @@ def row(extra_classes=""):
 @register.simple_tag
 def endrow():
   return format_html("</div>")
+
+
+@register.simple_tag
+def col():
+  return format_html('<div class="col">')
+
+
+@register.simple_tag
+def endcol():
+  return format_html("</div>")
+
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+  posts = Post.objects.exclude(pk=post.pk)[:]
+  return {"title": "Recent Posts", "posts": posts}
